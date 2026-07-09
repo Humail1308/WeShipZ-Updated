0c783c3aae097c4d2628f78d1b1d53a34212f0f1
Humail1308 <baazil1308@gmail.com>
2026-07-09 00:48:39 -0700

    add custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Home } from './pages/Home';
+import { CustomCursor } from './components/ui/CustomCursor';
 
 function App() {
   return (
     <div className="font-body-md text-body-md selection:bg-electric-blue selection:text-white min-h-screen">
+      <CustomCursor />
       <Home />
     </div>
   );
